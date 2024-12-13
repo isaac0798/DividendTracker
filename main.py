35b3150b7fa0472013ed14e9ce88dad1c1e734b1
Isaac Tennant <isaactennant@Isaacs-MacBook-Pro.local>
2024-12-13 21:45:29 +0000

    convert data

--- main.py
+++ main.py
@@ -6,11 +6,78 @@ from collections import defaultdict
 
 st.write('Dividend Tracker')
 
+def getYearFromTime(time):
+    timeio = time.split()
+    YYYYMMDD = timeio[0].split('-')
+    
+    return int(YYYYMMDD[0])
+
+def getMonthFromTime(time):
+    timeio = time.split()
+    YYYYMMDD = timeio[0].split('-')
+    
+    return int(YYYYMMDD[1])
+
+def getDayFromTime(time):
+    timeio = time.split()
+    YYYYMMDD = timeio[0].split('-')
+    
+    return int(YYYYMMDD[2])
+
 tickerTotal = defaultdict(float)
+divvies = {}
 df = pandas.read_csv('from_2024-05-01_to_2024-12-13.csv')
 
 for index, row in df.iterrows():
     tickerTotal[row['Ticker']] += row['Total']
+    
+    month = getMonthFromTime(row['Time'])
+    day = getDayFromTime(row['Time'])
+    year = getYearFromTime(row['Time'])
+    
+    if year in divvies:
+        if month in divvies[year]:
+            if day in divvies[year][month]:
+                if row['Ticker'] in divvies[year][month][day]:
+                    # multiple payment scenario
+                    print('multiple payment issue')
+                else:
+                    divvies[year][month][day][row['Ticker']] = {
+                        'name': row['Name'],
+                        'time': row['Time'],
+                        'total': row['Total']
+                    }
+            else:
+                divvies[year][month][day] = {
+                        row['Ticker']: {
+                            'name': row['Name'],
+                            'time': row['Time'],
+                            'total': row['Total']
+                        }
+                }
+        else:
+            divvies[year][month] = {
+                    day: {
+                        row['Ticker']: {
+                            'name': row['Name'],
+                            'time': row['Time'],
+                            'total': row['Total']
+                        }
+                    }
+                }
+    else:
+        divvies[year] = {
+            month: {
+                day: {
+                    row['Ticker']: {
+                        'name': row['Name'],
+                        'time': row['Time'],
+                        'total': row['Total']
+                    }
+                }
+            }
+        }
+        
 
 #print(tickerTotal)
 
@@ -21,12 +88,7 @@ for k, v in tickerTotal.items():
 #print(round(totalVal, 2))
 
 st.write('Total', round(totalVal, 2))
-
-print(df)
-
-options = ["North", "East", "South", "West"]
-selection = st.pills("Directions", options, selection_mode="multi")
-st.markdown(f"Your selected options: {selection}.")
-
 st.bar_chart(tickerTotal)
+
+st.write(divvies)
 st.button("Re-run")
