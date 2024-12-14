d7d4743c6dead2b11a7358985d13ea2066716fb3
Isaac Tennant <isaactennant@Isaacs-MacBook-Pro.local>
2024-12-14 09:38:25 +0000

    monthly percentages

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from collections import defaultdict
 
 st.write('Dividend Tracker')
 
+uploaded_file = st.file_uploader("Choose a file")
+
 def getYearFromTime(time):
     timeio = time.split()
     YYYYMMDD = timeio[0].split('-')
@@ -24,41 +26,54 @@ def getDayFromTime(time):
     
     return int(YYYYMMDD[2])
 
-tickerTotal = defaultdict(float)
-divvies = {}
-df = pandas.read_csv('from_2024-05-01_to_2024-12-13.csv')
+if uploaded_file is not None:
+    tickerTotal = defaultdict(float)
+    divvies = {}
+    df = pandas.read_csv(uploaded_file)
 
-for index, row in df.iterrows():
-    tickerTotal[row['Ticker']] += row['Total']
-    
-    month = getMonthFromTime(row['Time'])
-    day = getDayFromTime(row['Time'])
-    year = getYearFromTime(row['Time'])
-    
-    if year in divvies:
-        if month in divvies[year]:
-            if day in divvies[year][month]:
-                if row['Ticker'] in divvies[year][month][day]:
-                    # multiple payment scenario
-                    print('multiple payment issue')
-                else:
-                    divvies[year][month][day][row['Ticker']] = {
-                        'name': row['Name'],
-                        'time': row['Time'],
-                        'total': row['Total'],
-                        'ticker': row['Ticker']
-                    }
-            else:
-                divvies[year][month][day] = {
-                        row['Ticker']: {
+    for index, row in df.iterrows():
+        tickerTotal[row['Ticker']] += row['Total']
+        
+        month = getMonthFromTime(row['Time'])
+        day = getDayFromTime(row['Time'])
+        year = getYearFromTime(row['Time'])
+        
+        if year in divvies:
+            if month in divvies[year]:
+                if day in divvies[year][month]:
+                    if row['Ticker'] in divvies[year][month][day]:
+                        # multiple payment scenario
+                        print('multiple payment issue')
+                    else:
+                        divvies[year][month][day][row['Ticker']] = {
                             'name': row['Name'],
                             'time': row['Time'],
                             'total': row['Total'],
                             'ticker': row['Ticker']
                         }
-                }
+                else:
+                    divvies[year][month][day] = {
+                            row['Ticker']: {
+                                'name': row['Name'],
+                                'time': row['Time'],
+                                'total': row['Total'],
+                                'ticker': row['Ticker']
+                            }
+                    }
+            else:
+                divvies[year][month] = {
+                        day: {
+                            row['Ticker']: {
+                                'name': row['Name'],
+                                'time': row['Time'],
+                                'total': row['Total'],
+                                'ticker': row['Ticker']
+                            }
+                        }
+                    }
         else:
-            divvies[year][month] = {
+            divvies[year] = {
+                month: {
                     day: {
                         row['Ticker']: {
                             'name': row['Name'],
@@ -68,61 +83,76 @@ for index, row in df.iterrows():
                         }
                     }
                 }
-    else:
-        divvies[year] = {
-            month: {
-                day: {
-                    row['Ticker']: {
-                        'name': row['Name'],
-                        'time': row['Time'],
-                        'total': row['Total'],
-                        'ticker': row['Ticker']
-                    }
-                }
             }
-        }
+            
+
+    #print(tickerTotal)
+
+    totalVal = 0
+    for k, v in tickerTotal.items():
+        totalVal += v
         
+    #print(round(totalVal, 2))
 
-#print(tickerTotal)
+    st.write('Total', round(totalVal, 2))
+    st.bar_chart(tickerTotal)
 
-totalVal = 0
-for k, v in tickerTotal.items():
-    totalVal += v
+    yearSelected = st.selectbox(
+        "Pick Year",
+        divvies.keys(),
+    )
+
+    monthSelected = st.selectbox(
+        "Pick Month",
+        divvies[year].keys(),
+    )
+
+    st.write("You selected:", yearSelected, monthSelected)
+
+
+    monthlyTickerTotal = defaultdict(float)
+    def getTotalValuesFromMonth(monthlyDivvies):
+        for dailyDivvies in monthlyDivvies:
+            for div in monthlyDivvies[dailyDivvies]:
+                monthlyTickerTotal[div] = monthlyDivvies[dailyDivvies][div]['total']
+
+
+    getTotalValuesFromMonth(divvies[yearSelected][monthSelected])
+
+    monthlyVal = 0
+    for k, v in monthlyTickerTotal.items():
+        monthlyVal += v
+
+    st.write('Total', round(monthlyVal, 2))
     
-#print(round(totalVal, 2))
+    def getTotalValueFromDate():
+        uptoDivvies = {}
+        for year in divvies.keys():
+            if year not in uptoDivvies:
+                uptoDivvies[year] = {}
+            
+            if year == yearSelected:
+                for month in divvies[year].keys():
+                    if month not in uptoDivvies[year]:
+                        uptoDivvies[year][month] = {}
+            
+                    if monthSelected == month:
+                        break
+                    else:
+                        uptoDivvies[year][month] = divvies[year][month]
 
-st.write('Total', round(totalVal, 2))
-st.bar_chart(tickerTotal)
-
-year = st.selectbox(
-    "Pick Year",
-    divvies.keys(),
-)
-
-month = st.selectbox(
-    "Pick Month",
-    divvies[year].keys(),
-)
-
-st.write("You selected:", year, month)
-
-
-monthlyTickerTotal = defaultdict(float)
-def getTotalValuesFromMonth(monthlyDivvies):
-    for dailyDivvies in monthlyDivvies:
-        for div in monthlyDivvies[dailyDivvies]:
-            print(div)
-            print(monthlyDivvies[dailyDivvies][div])
-            #monthlyTickerTotal[monthlyDivvies[dailyDivvies][div]] = monthlyDivvies[dailyDivvies][div]
-            monthlyTickerTotal[div] = monthlyDivvies[dailyDivvies][div]['total']
-
-
-getTotalValuesFromMonth(divvies[year][month])
-
-monthlyVal = 0
-for k, v in monthlyTickerTotal.items():
-    monthlyVal += v
-
-st.write('Total', round(monthlyVal, 2))
-st.bar_chart(monthlyTickerTotal)
-st.button("Re-run")
+        return uptoDivvies
+                        
+    divviesUptoMonth = getTotalValueFromDate()
+    startingValueFromMonth = 0
+    for year in divviesUptoMonth:
+        for month in divviesUptoMonth[year]:
+            for day in divviesUptoMonth[year][month].keys():
+                for divvie in divviesUptoMonth[year][month][day].keys():
+                    startingValueFromMonth += divviesUptoMonth[year][month][day][divvie]['total']
+                        
+    
+    st.write('you started with that month with', round(startingValueFromMonth, 2))
+    st.write('with a monthly increase of: ', round((monthlyVal / startingValueFromMonth),2 ))
+    st.bar_chart(monthlyTickerTotal)
+    st.button("Re-run")
